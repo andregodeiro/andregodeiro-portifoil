21bfdf5b032c4d7834a689606cc9c2edc962728f
André Godeiro <andrevictor.godeiro@gmail.com>
2022-11-29 18:42:53 -0300

    main styles

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -4,6 +4,7 @@ export const ContainerMain = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin-top: 100px;
 `;
 
 export const ContainerTitle = styled.div``;
@@ -12,8 +13,12 @@ export const ContainerImage = styled.div``;
 
 export const Title = styled.h1`
   color: white;
+  font-size: 52px;
+  line-height: 24px;
 `;
 
 export const SubTitle = styled.h3`
   color: black;
+  font-size: 26px;
+  line-height: 24px;
 `;
